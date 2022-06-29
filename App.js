5616331f1d2fe568aaadb536a65a8f3b93ca5987
BenAflec <rafaelcardoso762@gmail.com>
2022-06-28 21:06:06 -0300

    Desafio App React-Native

---
+++ App.js
@@ -0,0 +1,84 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+
+
+const App = ( ) => {
+ const [toggle, setToggle] = useState (false);
+
+ const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+ useEffect(() => {
+   Torch.switchState(toggle);
+ }, [toggle]);
+
+ useEffect(() => {
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+
+    return () => subscription.remove();
+  });
+
+  return (
+  <View style={toggle ? style.containerLight : style.container}>
+    <TouchableOpacity onPress={handleChangeToggle}>
+  <Image
+   style={toggle ? style.lightingOn : style.lightingOff}
+   source={
+    toggle
+      ? require('./assets/eco-light.png')
+      : require('./assets/eco-light-off.png')
+    }
+   />
+   <Image
+   style={style.diologo}
+   source={
+    toggle
+      ? require('./assets/logo-dio.png')
+      : require('./assets/logo-dio-white.png')
+   }
+  />
+  </TouchableOpacity>
+  </View>
+ );
+} ;
+
+export default App;
+
+const style = StyleSheet.create({
+ container: {
+  flex: 1,
+  backgroundColor: 'black',
+  alighItens: 'center',
+  justifyContent: 'center',
+ },
+ containerLight: {
+  flex: 1,
+  backgroundColor: 'white',
+  alignItens: 'center',
+  justifyContent: 'center',
+},
+lightingOn: {
+  resizeMode: 'contain',
+  alignSelf: 'center',
+  wight: 150,
+  height: 150,
+},
+lightingOff: {
+  resizeMode: 'contain',
+  alignSelf: 'center',
+  tintColor: 'white',
+  wight: 150,
+  height: 150,
+},
+diologo: {
+  resizeMode: 'contain',
+  alignSelf: 'center',
+  width: 250,
+  height: 250,
+},
+});
+
+
